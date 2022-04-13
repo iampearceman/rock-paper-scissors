21f8fbc4b37d7c01a84e09b7b9b32feb635119da
StarL0rdDude <63902456+StarL0rdDude@users.noreply.github.com>
2022-04-14 01:06:59 +0300

    few new things been added

--- scripts.js
+++ scripts.js
@@ -1,37 +1,63 @@
 const playOption = ["rock", "paper", "scissors"];
-
-
+let computerLivesCounter = 5;
+let userLivesCounter = 5;
 
 function computerPlay() {
     let randomChoice = Math.floor(Math.random() * playOption.length);
     return playOption[randomChoice];
 }
 
-function userPlay(){
+function userPlay() {
     let input = prompt("Please enter your choice: rock, paper, scissors").toLowerCase();
     if (playOption.includes(input)) {
         return input;
     } else {
-        alert("Please enter a valid option");
         return false;
     }
 }
 
 
-function checkWinner() {
+function playRound() {
     let computerSelection = computerPlay();
     console.log(`comp ${computerSelection}`);
     let playerSelection = userPlay();
+    if (!playerSelection) {
+        return playRound();
+    }
     console.log(`user ${playerSelection}`);
     if (computerSelection === playerSelection) {
-        return `it's a tie`;
+        return `Tie`;
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")
     ) {
-        return `User won!`;
+        return `User`;
     } else {
-        return `Computer won`;
+        return `Computer`;
     }
 }
+
+
+
+function game() {
+    while (computerLivesCounter > 0 && userLivesCounter > 0) {
+        let winner = playRound();
+        if (winner === `User`) {
+            computerLivesCounter--;
+        } else if (winner === `Computer`) {
+            userLivesCounter--;
+        } else {
+            playRound();
+        } 
+        console.log(`user lives = ${userLivesCounter}`, `computer lives = ${computerLivesCounter}`);
+    } 
+
+    if (computerLivesCounter > userLivesCounter) {
+        return `Computer Won!`;
+    }else {
+        return `User won!`;
+    }    
+}
+
+
